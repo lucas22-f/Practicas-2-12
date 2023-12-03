4c8504a57fbbce393da968e9009aaff914f1a02d
Lucas <lucas.200061@gmail.com>
2023-12-02 23:51:34 -0300

    I02 , I03

---
+++ EjercicioI02/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Ingresar un número y mostrar el cuadrado y el cubo del mismo. Se
+        //debe validar que el número sea mayor que cero, caso contrario,
+        //mostrar el mensaje: "ERROR. ¡Reingresar número!".
+
+        Console.WriteLine("ingrese un numero");
+        int numero = int.Parse(Console.ReadLine());
+        int cuadrado = 0;
+        double cubo = 0;
+        if (numero > 0){
+
+            cuadrado = (int)Math.Pow(numero, 2);
+            cubo = Math.Pow(numero , 3);
+        }
+        else
+        {
+            Console.WriteLine("ERROR , reingresar numero");
+        }
+
+        Console.WriteLine("cuadrado :" + cuadrado);
+        Console.WriteLine("cubo :" + cubo);
+
+
+    }
+}
---
+++ EjercicioI03/Program.cs
@@ -0,0 +1,56 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el usuario por consola
+        // Validar que el dato ingresado por el usuario sea un número
+        // Volver a pedir el dato hasta que sea válido o el usuario ingrese "salir
+        // Si ingresa "salir", cerrar la consola
+        // Al finalizar, preguntar al usuario si desea volver a operar.Si la respuesta es afirmativa, iterar.De lo contrario, cerrar la consola.
+
+
+        int numero = 0;
+        Console.WriteLine("ingresar un numero para calcular los numeros primos");
+        string ingreso  = Console.ReadLine();
+       
+        if(int.TryParse(ingreso, out numero))
+        {
+            
+            for (int i = 1; i <= numero; i++)
+            {
+                if (i >= 2)
+                {
+                    bool divisible = false;
+
+                    for(int  j = 2; j < i; j++)
+                    {
+                        if(i%j == 0)
+                        {
+                            divisible = true;
+                            break;
+                        }
+                    }
+
+                    if (!divisible)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+
+
+
+    }
+}
