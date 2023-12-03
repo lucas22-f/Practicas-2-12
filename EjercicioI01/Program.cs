1475f099c803d998b9782bafffabf6cb2a45c688
Lucas <lucas.200061@gmail.com>
2023-12-02 21:43:41 -0300

    I01

--- EjercicioI01/Program.cs
+++ EjercicioI01/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero = 0;
+        int maximo = int.MinValue;
+        int minimo = int.MaxValue;
+        int promedio = 0;
+        for (int i = 0; i < 5; i++) {
+
+            Console.WriteLine("ingrese numeros ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero < minimo)
+            {
+                minimo = numero;
+            }
+            else
+            {
+                maximo = numero;
+            }
+
+            promedio += numero;
+        }
+
+        promedio = promedio / 5;
+        Console.WriteLine("promedio : " + promedio);
+        Console.WriteLine("maximo : " + maximo);
+        Console.WriteLine("minimo : " + minimo);
+
+
+
+    }
+}
